d98cb1c1756901c87d1f5b51943509cdd727291a
Mike <bobopower@gmail.com>
2021-07-31 22:54:09 -0400

    Add Arduino code that reads USB input and sends hardware keystrokes via USB output

---
+++ arduino/keyboard/keyboard.ino
@@ -0,0 +1,31 @@
+#include "Keyboard.h"
+
+const int PRESS = 1;
+const int RELEASE = 2;
+//int TYPE = 1;
+
+void setup() {from 
+  // open the serial port:
+  Serial.begin(9600);
+  // initialize control over the keyboard:
+  Keyboard.begin();
+}
+
+void loop() {
+  // check for incoming serial data:
+  if (Serial.available() > 0) {
+    // read incoming serial data
+    // data should be 3 bytes: [character, press/release byte, null]
+    byte input[3];
+    Serial.readBytesUntil(0, input, 3);
+    char character = input[0];
+    switch (input[1]) {
+      case PRESS:
+        Keyboard.press(character);
+        break;
+       case RELEASE:
+        Keyboard.release(character);
+        break;
+    }
+  }
+}
